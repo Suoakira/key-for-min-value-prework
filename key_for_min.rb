95930aabed8e9ee90273b7af0636eca52f96c92a
Stephen Courtier <steves0089@gmail.com>
2018-09-11 15:34:30 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   lowest_number = nil #setting the lowest number to undefined
   lowest_key = nil
-  name_hash.each do |key, value|
+  name_hash.collect do |key, value|
     if lowest_number == nil || lowest_number > value # lowest_number = nil on left as processed first and redefined
       lowest_number = value
       lowest_key = key
